e12e22a517969e6d2879e5dfc9d5d17acf9dd632
FrmJ <70322094+FrmJ@users.noreply.github.com>
2025-09-19 20:19:56 -0700

    feat: ask for user to choose from rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const getComputerChoice = () => {
     return Math.floor(Math.random() * 3);
 };
+
+const getHumanChoice = () => {
+    const promptText = "Choose a number corresponding to the option:\nRock - 0\nPaper - 1\nScissor - 2";
+    return parseInt(prompt(promptText));
+};
